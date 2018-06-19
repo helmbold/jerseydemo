2bab801eb067cb696c9d67bb692fcc64f373fe0d
Helmbold-External, Christian <Christian.Helmbold-External@durr.com>
2018-06-19 11:04:12 +0200

    remove WebTestClient from Test and WebFlux from dependencies (now working)

--- src/test/kotlin/com/example/jerseydemo/ResourceTest.kt
+++ src/test/kotlin/com/example/jerseydemo/ResourceTest.kt
@@ -4,10 +4,8 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import org.springframework.test.web.reactive.server.WebTestClient
 import javax.ws.rs.client.ClientBuilder
 
 
@@ -15,14 +13,6 @@ import javax.ws.rs.client.ClientBuilder
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ResourceTest {
-
-    @Autowired
-    lateinit var client: WebTestClient
-
-    @Test
-    fun getTest() {
-        client.get().uri("demo").exchange().expectStatus().isOk
-    }
 
     @Test
     fun testWithJerseyClient() {
